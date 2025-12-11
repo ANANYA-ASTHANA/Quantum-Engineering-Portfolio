ef364294d1b67af4603765d2f31e0f32fbba5872
Ananya Asthana <119975752+ANANYA-ASTHANA@users.noreply.github.com>
2025-12-11 11:23:03 +0400

    Update user1_e91.py

--- 3_Project/proj_code/quantum/user1_e91.py
+++ 3_Project/proj_code/quantum/user1_e91.py
@@ -31,8 +31,7 @@ def alice_e91(num_bits=7000):
             if basis == 1:
                 q.H()  # X-basis
             elif basis == 2:
-                q.H()  
-                q.Y()  # Y-basis
+                q.K()  # Y-basis
 
             # Measure the qubit
             raw_key_bits.append(q.measure())
